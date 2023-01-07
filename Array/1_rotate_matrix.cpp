d0fba58406bb0285956625aae59fa8ebd81b2e5b
Priyanshu <priyanshukingh@gmail.com>
2023-01-07 19:49:34 +0530

    episode 1

--- Array/1_rotate_matrix.cpp
+++ Array/1_rotate_matrix.cpp
@@ -1,1 +1,11 @@
-#include<bits/stdc++.h>
+void Solution::rotate(vector<vector<int> > &A) {
+    for(int i = 0;i < A.size();i++){
+        for(int j = i+1;j <A.size();j++){
+            swap(A[i][j],A[j][i]);
+        }
+    }
+    for(int i = 0;i < A.size();i++){
+        reverse(A[i].begin(),A[i].end());
+    }
+    
+}
